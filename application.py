99ded386fc45e7c0739645ef4b218d34ac0ed119
David Guyon <david@guyon.me>
2017-01-04 10:38:02 +0100

    Change: PNG is now showing the per-day consumption

--- application.py
+++ application.py
@@ -24,7 +24,33 @@ def generate_graph():
 
 
     x = [dt.datetime.strptime(d,'%d/%m/%Y').date() for d in dates]
-    # y = range(len(x)) # many thanks to Kyss Tao for setting me straight here
+
+    previous_day = None
+    previous_peak = None
+    previous_off_peak = None
+    date_axis = []
+    peak_axis = []
+    off_peak_axis = []
+    for day, peak, off_peak in zip(x, peak_consumptions, off_peak_consumptions):
+        if previous_day is None or previous_peak is None or previous_off_peak is None:
+            pass
+        else:
+            nb_days = (day-previous_day).days
+            delta_peak = peak - previous_peak
+            delta_off_peak = off_peak - previous_off_peak
+            peak_per_day = delta_peak / nb_days
+            off_peak_per_day = delta_off_peak / nb_days
+            print(nb_days, delta_peak, peak_per_day)
+            print(nb_days, delta_off_peak, off_peak_per_day)
+            for i in np.arange(0, nb_days):
+                date = previous_day + dt.timedelta(days=int(i))
+                date_axis.append(date)
+                peak_axis.append(peak_per_day)
+                off_peak_axis.append(off_peak_per_day)
+
+        previous_day = day
+        previous_peak = peak
+        previous_off_peak = off_peak
 
     def converter(x, _):
         """
@@ -38,19 +64,21 @@ def generate_graph():
     # first plot stands for the peak consumption
     fig, ax1 = plt.subplots(figsize=(9, 6))
     ax1.grid()
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+    ax1.set_ylim([0, 30])
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
     # plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    ax1.plot(x, peak_consumptions, 'bo-')
-    ax1.yaxis.set_major_formatter(FuncFormatter(converter))
-    ax1.set_ylabel('Énergie HP (KWh)', color='b')
+    ax1.plot(date_axis, peak_axis, 'b-')
+    # ax1.yaxis.set_major_formatter(FuncFormatter(converter))
+    ax1.set_ylabel('Énergie HP (Wh)', color='b')
     for tl in ax1.get_yticklabels():
         tl.set_color('b')
 
     # second plot stands for the off-peak consumption
     ax2 = ax1.twinx()
-    ax2.plot(x, off_peak_consumptions, 'go-')
-    ax2.yaxis.set_major_formatter(FuncFormatter(converter))
-    ax2.set_ylabel('Énergie HC (KWh)', color='g')
+    ax2.set_ylim([0, 30])
+    ax2.plot(date_axis, off_peak_axis, 'g-')
+    # ax2.yaxis.set_major_formatter(FuncFormatter(converter))
+    ax2.set_ylabel('Énergie HC (Wh)', color='g')
     for tl in ax2.get_yticklabels():
         tl.set_color('g')
     # align grids
